5ba790513d43fdd571e52c351cd7c5fbeba49e35
deivisjl <deivisjl@gmail.com>
2022-07-03 11:40:09 -0600

    Initial commit

---
+++ src/apis/Core.Api/Startup.cs
@@ -0,0 +1,73 @@
+﻿using Core.Api.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.DatabaseContext;
+using System.Security.Claims;
+
+namespace Core.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+            );
+
+            services.AddMyDependencies(Configuration);
+
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = Configuration["Auth:Url"];
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = Configuration["Auth:ApiName"];
+
+                    // Agregamos esto para especificar donde se encuentra el role
+                    options.RoleClaimType = ClaimTypes.Role;
+                });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowSpecificOrigin", builder =>
+                    builder.AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowAnyOrigin()
+                );
+            });
+
+            // Automapper config
+            MyMaps.Initialize();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseCors("AllowSpecificOrigin");
+
+            app.UseMvc();
+        }
+    }
+}
